8737f6db4ad8795c75bc5d845f370002cfcb85e5
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-26 16:57:36 +0800

    fixed vuh and added new cmake

---
+++ build/CMakeFiles/vuh.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vuh.dir/src/vuh/device.cpp.o"
+  "CMakeFiles/vuh.dir/src/vuh/device.cpp.o.d"
+  "CMakeFiles/vuh.dir/src/vuh/error.cpp.o"
+  "CMakeFiles/vuh.dir/src/vuh/error.cpp.o.d"
+  "CMakeFiles/vuh.dir/src/vuh/instance.cpp.o"
+  "CMakeFiles/vuh.dir/src/vuh/instance.cpp.o.d"
+  "CMakeFiles/vuh.dir/src/vuh/utils.cpp.o"
+  "CMakeFiles/vuh.dir/src/vuh/utils.cpp.o.d"
+  "lib/libvuh.dylib"
+  "lib/libvuh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vuh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
